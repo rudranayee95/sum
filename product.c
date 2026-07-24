bea7e1ea3e3cfa3187e5be107da3d7ba0bd73f2a
rudranayee95 <rudranayee95@gmail.com>
2026-07-25 00:22:46 +0530

    Add files via upload

---
+++ product.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter the value of first number :");
+    scanf("%d",&a);
+    printf("enter the value of second number :");
+    scanf("%d",&b);
+    c=a*b;
+    printf("the product of given number is :%d",c);
+    return 0;
+}
